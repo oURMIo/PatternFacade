c31952a240301083120ef3c394ab58bc8a789082
God <dimach@gmail.com>
2022-05-12 13:35:58 +0300

    last

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] arg){
         InfoGet student1 = new Student();
-        Teacher teacher1 = new Teacher();
+        InfoGet teacher1 = new Teacher();
 
         teacher1.printInfo();
-
         student1.printInfo();
     }
 //    private static void getInfo(Teacher obj){
